83e79fe9c4ece1eb8518ce7268052a0aa5d5bcc4
Deokgyu Yang <secugyu@gmail.com>
2022-02-24 01:39:39 +0900

    src: Allow only the validate data from all the endpoints
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    // Ensure that all data sent to the server is valid
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  )
+
   await app.listen(3000);
 }
 bootstrap();
