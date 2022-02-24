9b209f9bf41db4fbf36c0bcdb29614cf6a41c2ee
Deokgyu Yang <secugyu@gmail.com>
2022-02-24 17:04:03 +0900

    src: users: Add basic controller, service implements for managing users
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Put } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { RemoveUserDto } from './dto/remove-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Put('/create')
+  createUser(
+    @Body() createUserDto: CreateUserDto
+  ){
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Patch('/update')
+  updateUser(
+    @Body() updateUserDto: UpdateUserDto
+  ){
+    return this.usersService.updateUser(updateUserDto);
+  }
+
+  @Delete('/remove')
+  removeUser(
+    @Body() deleteUserDto: RemoveUserDto
+  ){
+    return this.usersService.removeUser(deleteUserDto);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { RemoveUserDto } from './dto/remove-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -10,19 +13,41 @@ export class UsersService {
     private userRepository: Repository<User>,
   ) {}
 
-  async createUser(): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
     const user = new User();
-    user.name = 'test';
-    user.email = '';
-    user.password = '';
+    user.email = createUserDto.email;
+    user.name = createUserDto.name;
+    user.password = createUserDto.password;
+
+    // Check if the user already exists
+    const existingUser = await this.userRepository.findOne(user.email);
+    if (existingUser) {
+      throw new UnprocessableEntityException('Email already exists');
+    }
+
     return await this.userRepository.save(user);
   }
 
-  async removeUser(id: number): Promise<void> {
-    await this.userRepository.delete(id);
+  async updateUser(updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findUser(updateUserDto.email);
+
+    user.name = updateUserDto.name;
+    user.password = updateUserDto.password;
+
+    return await this.userRepository.save(user);
   }
 
-  async findAll(): Promise<User[]> {
+  async removeUser(deleteUserDto: RemoveUserDto): Promise<User> {
+    const user = await this.findUser(deleteUserDto.email);
+
+    return await this.userRepository.remove(user);
+  }
+
+  async findUser(email: string): Promise<User> {
+    return await this.userRepository.findOne({ email: email });
+  }
+
+  async findAllUser(): Promise<User[]> {
     return await this.userRepository.find();
   }
 }
