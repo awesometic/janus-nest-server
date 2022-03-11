d5757681521f598185cfa19f3195a9d1f64fc725
Deokgyu Yang <secugyu@gmail.com>
2022-03-11 17:44:02 +0900

    src: Remove unused import
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
