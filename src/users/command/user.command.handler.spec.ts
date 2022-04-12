a61867d28dbd56b55365f1e33d7707c0eeff7e50
Deokgyu Yang <secugyu@gmail.com>
2022-04-13 01:52:36 +0900

    src: users: Add test code for user.command.handler
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

---
+++ src/users/command/user.command.handler.spec.ts
@@ -0,0 +1,215 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EmailSenderService } from 'src/email/email-sender.service';
+import { UserRepositoryWrapper } from '../repository/user.repository';
+import {
+  CreateUserCommand,
+  CreateUserCommandResult,
+  RemoveUserCommand,
+  RemoveUserCommandResult,
+  UpdateUserCommand,
+  UpdateUserCommandResult,
+  VerifyEmailCommand,
+  VerifyEmailCommandResult,
+} from './user.command';
+import {
+  CreateUserHandler,
+  UpdateUserHandler,
+  RemoveUserHandler,
+  VerifyEmailHandler,
+} from './user.command.handler';
+
+class MockUser {
+  constructor({
+    id,
+    email,
+    name,
+    password,
+    verifyToken,
+    status,
+  }: {
+    id: number;
+    email: string;
+    name: string;
+    password: string;
+    verifyToken: string;
+    status: number;
+  }) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.verifyToken = verifyToken;
+    this.status = status;
+  }
+
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  verifyToken: string;
+  status: number;
+}
+
+describe('UserCommandHandler', () => {
+  let createUserHandler: CreateUserHandler;
+  let updateUserHandler: UpdateUserHandler;
+  let removeUserHandler: RemoveUserHandler;
+  let verifyEmailHandler: VerifyEmailHandler;
+  let userRepositoryWrapper: UserRepositoryWrapper;
+  let emailSenderService: EmailSenderService;
+
+  let mockUser: MockUser;
+  const userId = 1;
+  const email = 'test@test.com';
+  const name = 'name';
+  const password = 'password';
+  const newPassword = 'newPassword';
+  const verifyToken = 'verifyToken';
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CreateUserHandler,
+        UpdateUserHandler,
+        RemoveUserHandler,
+        VerifyEmailHandler,
+        {
+          provide: UserRepositoryWrapper,
+          useValue: {
+            createUser: jest
+              .fn()
+              .mockImplementation((email, name, password) => {
+                mockUser = new MockUser({
+                  id: userId,
+                  email: email,
+                  name: name,
+                  password: password,
+                  verifyToken: verifyToken,
+                  status: 1,
+                });
+
+                return {
+                  id: mockUser.id,
+                  email: mockUser.email,
+                };
+              }),
+            updateUser: jest.fn().mockImplementation((_, __, password) => {
+              mockUser.password = password;
+
+              return {
+                id: mockUser.id,
+                email: mockUser.email,
+              };
+            }),
+            removeUser: jest.fn().mockImplementation(() => {
+              mockUser = undefined;
+
+              return {
+                id: userId,
+                email: email,
+              };
+            }),
+            checkUserExists: jest
+              .fn()
+              .mockImplementation((email) =>
+                mockUser ? email != mockUser.email : true,
+              ),
+            checkUserVerified: jest
+              .fn()
+              .mockImplementation(
+                (verifyToken) =>
+                  verifyToken == mockUser.verifyToken && mockUser.status == 0,
+              ),
+            verifyUserEmail: jest.fn().mockImplementation((verifyToken) => {
+              if (verifyToken == mockUser.verifyToken) {
+                mockUser.status = 0;
+              }
+
+              return {
+                id: mockUser.id,
+                email: mockUser.email,
+                verifyToken: mockUser.verifyToken,
+              };
+            }),
+          },
+        },
+        {
+          provide: EmailSenderService,
+          useValue: {
+            sendVerification: jest.fn().mockReturnValue(true),
+          },
+        },
+      ],
+    }).compile();
+
+    createUserHandler = module.get<CreateUserHandler>(CreateUserHandler);
+    updateUserHandler = module.get<UpdateUserHandler>(UpdateUserHandler);
+    removeUserHandler = module.get<RemoveUserHandler>(RemoveUserHandler);
+    verifyEmailHandler = module.get<VerifyEmailHandler>(VerifyEmailHandler);
+    userRepositoryWrapper = module.get<UserRepositoryWrapper>(
+      UserRepositoryWrapper,
+    );
+    emailSenderService = module.get<EmailSenderService>(EmailSenderService);
+  });
+
+  describe('createUser', () => {
+    it('should create user', async () => {
+      const commandResults: CreateUserCommandResult = {
+        userId: userId,
+        email: email,
+      };
+      const executeResults = await createUserHandler.execute(
+        new CreateUserCommand(email, name, password),
+      );
+
+      expect(executeResults).toEqual(commandResults);
+    });
+  });
+
+  describe('updateUser', () => {
+    it("should update user's password", async () => {
+      const commandResults: UpdateUserCommandResult = {
+        userId: userId,
+        email: email,
+      };
+      const executeResults = await updateUserHandler.execute(
+        new UpdateUserCommand(email, name, newPassword),
+      );
+
+      expect(executeResults).toEqual(commandResults);
+      expect(mockUser.password).toEqual(newPassword);
+    });
+  });
+
+  describe('verifyEmail', () => {
+    it("should verify user's email", async () => {
+      const commandResults: VerifyEmailCommandResult = {
+        userId: userId,
+        email: email,
+        verifyToken: verifyToken,
+      };
+      const executeResults = await verifyEmailHandler.execute(
+        new VerifyEmailCommand(verifyToken),
+      );
+
+      expect(executeResults).toEqual(commandResults);
+      expect(mockUser.status).toEqual(0);
+    });
+  });
+
+  // TODO: Currently, this test should be the last one to keep the mockUser alive
+  describe('removeUser', () => {
+    it('should remove user', async () => {
+      const commandResults: RemoveUserCommandResult = {
+        userId: userId,
+        email: email,
+      };
+      const executeResults = await removeUserHandler.execute(
+        new RemoveUserCommand(email, newPassword),
+      );
+
+      expect(executeResults).toEqual(commandResults);
+      expect(mockUser).toBeUndefined();
+    });
+  });
+});
